807ffa44fd93f407a9275c5a87f424da96bfc82e
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-04-29 18:58:53 +0300

    feat: done hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,24 +1,36 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Calculate turns number and time (in seconds) required
  * to solve puzzle
- * 
+ *
  * @param {Number} disks number of disks
  * @param {Number} turnsSpeed speed (in turns/hour)
  * @return {Object} object with props turns (number of turns)
  * and seconds (time in seconds)
  *
  * @example
- * 
+ *
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
+function calculateHanoi(disksNumber, turnsSpeed) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let result = {
+    turns: null,
+    seconds: null,
+  };
+
+  result.turns = Math.pow(2, disksNumber) - 1;
+  result.seconds = Math.floor((3600 / turnsSpeed) * result.turns);
+
+  //console.log(result);
+  return result;
 }
 
+//console.log(calculateHanoi(9, 4308));
+
 module.exports = {
-  calculateHanoi
+  calculateHanoi,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,47 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(dateIn) {
+  if (!dateIn) {
+    return "Unable to determine the time of year!";
+  }
+
+  if (
+    new Date(dateIn).toString() == "Invalid Date" ||
+    new Date(dateIn).toString() ==
+      "Thu Jan 01 1970 08:36:32 GMT+0300 (Москва, стандартное время)"
+  ) {
+    return new Error("Invalid date!");
+  }
+
+  let seasons = ["spring", "summer", "autumn", "winter"];
+
+  if (dateIn.getMonth() <= 1) {
+    return seasons[3];
+  } else if (dateIn.getMonth() <= 4) {
+    return seasons[0];
+  } else if (dateIn.getMonth() <= 7) {
+    return seasons[1];
+  } else if (dateIn.getMonth() <= 10) {
+    return seasons[2];
+  } else {
+    return seasons[3];
+  }
 }
 
+//console.log(getSeason(20192701));
+//console.log(new Date(20192701).toString());
+
 module.exports = {
-  getSeason
+  getSeason,
 };
