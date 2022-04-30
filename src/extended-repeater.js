ca1d5cc93dd186ca09668511048e3951e76a1b82
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-04-30 11:41:29 +0300

    feat: partially done transform-array and extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,83 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+
+  if (!!options.addition || options.addition == false) {
+    options.addition.toString();
+  }
+
+  if (!options.separator) {
+    options.separator = "+";
+  }
+
+  if (!options.additionSeparator) {
+    options.additionSeparator = "|";
+  }
+
+  if (!options.repeatTimes) {
+    options.repeatTimes = 1;
+  }
+
+  const additionRepeat = (
+    addition,
+    additionRepeatTimes = 1,
+    additionSeparator
+  ) => {
+    if (!addition && addition != false) {
+      return "";
+    }
+    let resultAdditionRepeat = "";
+    for (let i = 1; i <= additionRepeatTimes; ) {
+      resultAdditionRepeat += `${addition}`;
+      i++;
+      if (i <= additionRepeatTimes) {
+        resultAdditionRepeat += `${additionSeparator}`;
+      }
+    }
+    return resultAdditionRepeat;
+  };
+
+  let result = "";
+  for (let i = 1; i <= options.repeatTimes; ) {
+    result += `${str}${additionRepeat(
+      options.addition,
+      options.additionRepeatTimes,
+      options.additionSeparator
+    )}`;
+    i++;
+    if (i <= options.repeatTimes) {
+      result += `${options.separator}`;
+    }
+  }
+
+  return result;
 }
 
+console.log(
+  repeater(null, {
+    repeatTimes: 3,
+    separator: "??? ",
+    addition: null,
+    additionRepeatTimes: 3,
+    additionSeparator: "!!!",
+  })
+);
+
 module.exports = {
-  repeater
+  repeater,
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,75 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error(`'arr' parameter must be an instance of the Array!`);
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == "--double-next") {
+      if (arr[i + 1]) {
+        arr[i] = arr[i + 1];
+      } else {
+        arr.splice(i, 1);
+      }
+      console.log(arr);
+    } else if (arr[i] == "--double-prev") {
+      if (arr[i - 1]) {
+        arr[i] = arr[i - 1];
+      } else {
+        arr.splice(i, 1);
+      }
+    } else if (arr[i] == "--discard-next") {
+      if (arr[i + 1]) {
+        arr.splice(i + 1, 1);
+        arr.splice(i, 1);
+      } else {
+        arr.splice(i, 1);
+      }
+    } else if (arr[i] == "--discard-prev") {
+      if (arr[i - 1]) {
+        arr.splice(i - 1, 1);
+        arr.splice(i - 1, 1);
+      } else {
+        arr.splice(i, 1);
+      }
+      console.log(arr);
+    }
+  }
+
+  while (arr.indexOf("--double-prev") > -1) {
+    arr.splice(arr.indexOf("--double-prev"), 1);
+  }
+  while (arr.indexOf("--discard-prev") > -1) {
+    arr.splice(arr.indexOf("--discard-prev"), 1);
+  }
+  while (arr.indexOf("--discard-next") > -1) {
+    arr.splice(arr.indexOf("--discard-next"), 1);
+  }
+  while (arr.indexOf("--double-next") > -1) {
+    arr.splice(arr.indexOf("--double-next"), 1);
+  }
+
+  return arr;
 }
 
+/*console.log(
+  transform([1, 2, 3, "--double-next", 1337, "--discard-prev", 4, 5])
+);*/
+
 module.exports = {
-  transform
+  transform,
 };
