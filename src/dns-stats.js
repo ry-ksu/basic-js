036aaab7bbd29cdc600dc900b1bf289a3207563f
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-05-02 21:35:38 +0300

    feat: done dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let domainsRevers = [];
+  let result = {};
+
+  for (let i = 0; i < domains.length; i++) {
+    domainsRevers.push(domains[i].split(".").reverse());
+  }
+
+  for (let i = 0; i < domainsRevers.length; i++) {
+    let domainPiece = "";
+    for (let j = 0; j < domainsRevers[i].length; j++) {
+      domainPiece += `.${domainsRevers[i][j]}`;
+
+      result[domainPiece] = result[domainPiece] ? result[domainPiece] + 1 : 1;
+    }
+  }
+  return result;
 }
 
+//console.log(getDNSStats(["code.yandex.ru", "music.yandex.ru", "yandex.ru"]));
+
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
